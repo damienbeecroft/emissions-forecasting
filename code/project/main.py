6eed557a123996972d24ce69a2ef1a85eb69306e
DAMIEN BEECROFT <dob1998@klone-login03.hyak.local>
2024-09-23 14:36:24 -0700

    9-23-24: Worked on ARIMA forecasting for atmospheric pollutant data.

---
+++ code/project/main.py
@@ -0,0 +1,9 @@
+
+if __name__ == "__main__":
+    aggregator1_names = ['Farming and agriculture', 'Primary fossil fuels', 'Ores and minerals', 'Food products and tobacco', 'Fabric and paper products', 'Secondary fossil fuels', 
+'Petroleum products', 'Nuclear fuel', 'Plastics', 'Fetilisers', 'Chemicals, charcoal, and additives/blenders', 'Biofuels', 'Non-metallic mineral products', 'Metals and alloys',
+'Foundry work services', 'Metal products', 'Furniture and other goods n.e.c., raw materials, and bottles', 'Electricity', 'Electricity services', 'Other gas types', 'Water services',
+'Construction rervices and recycling', 'Trade and services', 'Incineration', 'Biogasification and land application', 'Composting and land application', 'Waste water treatment',
+'Landfill', 'Other services and households with employees']
+    agggregator1_indxs = [0,19,31,42,54,63,75,84,85,87,89,92,95,103,115,116,123,127,139,141,146,149,151,175,182,185,187,189,195,200]
+    print(len(aggregator1_names))
